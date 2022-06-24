98b81ea3cede2bc46422a56bcba815a503d5e89d
Dustin Ingram <di@users.noreply.github.com>
2022-06-24 17:41:45 +0000

    Handle missing SENTRY_DSN env var

--- inspector/main.py
+++ inspector/main.py
@@ -11,12 +11,12 @@ from flask import Flask, Response, abort, redirect, render_template, request
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-SENTRY_DSN = os.environ["SENTRY_DSN"]
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0,
-)
+if SENTRY_DSN := os.environ.get("SENTRY_DSN"):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[FlaskIntegration()],
+        traces_sample_rate=1.0,
+    )
 
 app = Flask(__name__)
 
