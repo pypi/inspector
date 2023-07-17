9ea1a0c16466029497f19236562374162d638a41
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2023-07-17 12:04:47 -0400

    Added file analysis summary + other improvements (#135)
    * Extracted symbols from main.py into their own modules.
    
    * Add analysis summary
    
    * reformat
    
    * lint

---
+++ inspector/analysis/checks.py
@@ -0,0 +1,35 @@
+from hashlib import sha256
+from typing import Any, Generator
+
+from inspector.analysis.codedetails import Detail, DetailSeverity
+from inspector.analysis.entropy import shannon_entropy
+from inspector.distribution import TarGzDistribution, ZipDistribution
+
+
+def __is_compiled(filepath: str) -> bool:
+    return filepath.endswith(".pyc") or filepath.endswith(".pyo")
+
+
+def basic_details(
+    distribution: TarGzDistribution | ZipDistribution, filepath: str
+) -> Generator[Detail, Any, None]:
+    contents = distribution.contents(filepath)
+
+    yield Detail(
+        severity=DetailSeverity.NORMAL,
+        prop_name="SHA-256",
+        value=sha256(contents).hexdigest(),
+    )
+
+    entropy = shannon_entropy(contents)
+    ent_suspicious = entropy > 6.0
+    yield Detail(
+        severity=DetailSeverity.HIGH if ent_suspicious else DetailSeverity.NORMAL,
+        prop_name="Entropy",
+        value=str(entropy) + " (HIGH)" if ent_suspicious else str(entropy),
+    )
+
+    if __is_compiled(filepath):
+        yield Detail(
+            severity=DetailSeverity.MEDIUM, prop_name="Compiled Python Bytecode"
+        )
---
+++ inspector/analysis/entropy.py
@@ -0,0 +1,14 @@
+import collections
+
+from math import log2
+
+
+def shannon_entropy(X: bytes):
+    result = 0.0
+
+    items = collections.Counter(X).items()
+    for b, count in items:
+        pr = count / len(X)
+        result -= pr * log2(pr)
+
+    return result
